8ec8f5ae6fc7f91c74e6da6f2f6d1a1433ad4db2
BNOECHO <74729129+BNOECHO@users.noreply.github.com>
2020-12-07 20:15:45 +0800

    Transer
    將環保署的檔案進行轉換

---
+++ Transer/Transer/Transer/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Transer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "讀取檔案";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "輸出檔案";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Location = new System.Drawing.Point(108, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(254, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "環保署光化資料轉換器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Transer/Transer/Transer/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace Transer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> ReadFile;
+        Dictionary<string,List< List<string>>> TDATA;
+        string TBOX1S = "";
+
+        List<string> CSVLSpliter(string s)
+        {
+            List<string> SA = new List<string>();
+            s += "\n";
+            string temp = "";
+            bool SKIPER = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]=='\n')
+                {
+                    SA.Add(temp);
+                }
+                else if (SKIPER)
+                {
+                    if (s[i] == '\"' && s[i + 1] == ',')
+                    {
+                        SKIPER = false;
+                        SA.Add(temp);
+                        temp = "";
+                        i++;
+                    }
+                    else
+                    {
+                        temp += s[i];
+                    }
+
+                }
+                else if (s[i] == ',' && s[i + 1] == '\"')
+                {
+                    SKIPER = true;
+                    SA.Add(temp);
+                    temp = "";
+                    i++;
+                }
+                else if (s[i] == ',')
+                {
+                    SA.Add(temp);
+                    temp = "";
+                }
+                else
+                {
+                    temp += s[i];
+                }
+
+
+            }
+
+            
+
+            return SA;
+        }
+        
+        private void Transer()
+        {
+            TBOX1S = "";
+            TDATA = new Dictionary<string, List< List<string>>>();
+            ReadFile.RemoveAt(0);
+            foreach (string Line in ReadFile)
+            {
+                List<string> SPLine =CSVLSpliter(Line);
+                if (!TDATA.Keys.Contains(SPLine[5]))//如果沒有該日期資料
+                {
+                    
+                    List<List<string>> L = new List<List<string>>();
+                    for (int i = 0; i < 24; i++)
+                    {
+                        L.Add(new List<string>());
+                        for (int o = 0; o < 56; o++) L[i].Add("");
+                    }
+                    TDATA.Add(SPLine[5], L);
+
+                }
+                for (int i = 0; i < 24; i++)/*if(SPLine[6 + i]!="nan")*/ TDATA[SPLine[5]][i][Convert.ToInt32(SPLine[2]) - 1] = SPLine[6 + i];
+            }
+
+            foreach (string K in TDATA.Keys)
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    TBOX1S += K + "," + i.ToString();
+                    for (int o = 0; o < 56; o++) TBOX1S += "," + TDATA[K][i][o];
+                    TBOX1S += "\n";
+                }
+
+
+            }
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "選擇CSV檔案";
+            ofd.Filter = "CSV Files (.csv)|*.csv|All Files(*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = ofd.FileName; 
+                string filename = ofd.FileName;
+                Encoding ec = Encoding.GetEncoding("big5");
+                ReadFile = File.ReadAllLines(filename, ec).ToList<string>();
+                Transer();
+               
+                
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog COFD = new CommonOpenFileDialog();
+            COFD.InitialDirectory=@"C:\";
+            COFD.IsFolderPicker = true;
+            COFD.Title = "選擇儲存路徑";
+            if (COFD.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+               FileStream FS= File.Create(COFD.FileName + @"\TEST1.csv");
+                StreamWriter SW = new StreamWriter(FS);
+                SW.Write(TBOX1S);
+                SW.Close();
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
