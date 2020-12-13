d311c0ea957d5cc2f837c3787e2dae4a264b1d14
BNOECHO <74729129+BNOECHO@users.noreply.github.com>
2020-12-13 20:07:36 +0800

    普通分支外加輸出站台選擇

---
+++ Transer/Transer 普通/Transer/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Transer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "讀取資料夾";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(10, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "輸出檔案";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(10, 222);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(535, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 250);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(535, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(99, 14);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(446, 196);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "讀取檔案";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "刪除項目";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "轉換檔案";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(551, 31);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(144, 242);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(549, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "輸出站臺";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(609, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "全選";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 285);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "環保署普通資料轉換器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Transer/Transer 普通/Transer/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace Transer
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> ReadFile;
+        Dictionary<string, List<List<string>>> TDATA;
+        CommonOpenFileDialog COFD = new CommonOpenFileDialog();
+        string OPdate = "";
+        List<string> OPstation = new List<string>();
+        List<string> CSVLSpliter(string s)
+        {
+            List<string> SA = new List<string>();
+            s += "\n";
+            string temp = "";
+            bool SKIPER = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]=='\n')
+                {
+                    SA.Add(temp);
+                }
+                else if (SKIPER)
+                {
+                    if (s[i] == '\"' && s[i + 1] == ',')
+                    {
+                        SKIPER = false;
+                        SA.Add(temp);
+                        temp = "";
+                        i++;
+                    }
+                    else
+                    {
+                        temp += s[i];
+                    }
+
+                }
+                else if (s[i] == ',' && s[i + 1] == '\"')
+                {
+                    SKIPER = true;
+                    SA.Add(temp);
+                    temp = "";
+                    i++;
+                }
+                else if (s[i] == ',')
+                {
+                    SA.Add(temp);
+                    temp = "";
+                }
+                else
+                {
+                    temp += s[i];
+                }
+            }
+
+            
+
+            return SA;
+        }
+        
+        private void Transer(BackgroundWorker worker,EventArgs e)
+        { 
+            int RPG = 0;
+            TDATA = new Dictionary<string,List< List<string>>>();
+            foreach (string Line in ReadFile)
+            {
+                List<string> SPLine =CSVLSpliter(Line);
+                if (OPdate == "") OPdate = SPLine[6].Split('-')[0] + SPLine[6].Split('-')[1];//輸出資料年月標記
+                string Date = SPLine[6].Replace('-', '/');
+                if (!TDATA.Keys.Contains(SPLine[1]))
+                {
+                    TDATA.Add(SPLine[1], new List<List<string>>());
+                }
+                TDATA[SPLine[1]].Add(new List<string>());
+                TDATA[SPLine[1]][TDATA[SPLine[1]].Count - 1].Add(Date);
+                TDATA[SPLine[1]][TDATA[SPLine[1]].Count - 1].Add(SPLine[1]);
+                TDATA[SPLine[1]][TDATA[SPLine[1]].Count - 1].Add(SPLine[4]);
+                for (int i = 0; i < 24; i++)
+                {
+                  if (SPLine[4]== "RAINFALL" && (SPLine[7 + i]=="0"|| SPLine[7 + i] == "0.0"))TDATA[SPLine[1]][TDATA[SPLine[1]].Count - 1].Add("NR");
+                 else   TDATA[SPLine[1]][TDATA[SPLine[1]].Count - 1].Add(SPLine[7 + i]);
+                }
+                    backgroundWorker1.ReportProgress(++RPG);
+            }    
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog OPPath = new CommonOpenFileDialog();
+            OPPath.InitialDirectory = @"C:\";
+            OPPath.IsFolderPicker = true;
+            OPPath.Title = "選擇讀取路徑";
+            if (OPPath.ShowDialog()==CommonFileDialogResult.Ok)
+            {
+                DirectoryInfo directory = new DirectoryInfo(OPPath.FileName);
+                foreach (FileInfo path in directory.GetFiles("*.csv"))
+                {
+                   string new_file = path.DirectoryName + "\\" + path.Name;
+                   if(!listBox1.Items.Contains(new_file)) listBox1.Items.Add(new_file);
+                }
+            }
+
+        }
+        private void Outputer(BackgroundWorker worker, EventArgs e)
+        {
+            int RPG = 0;
+            string File_Head = "日期,測站,測項,00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23\n";
+            Dictionary<string, List<string>> Files = new Dictionary<string, List<string>>();
+            foreach (string station in OPstation)
+            {
+                Files.Add(station, new List<string>());
+                Files[station].Add(File_Head);
+                foreach (List<string> Line in TDATA[station])
+                {
+                    string NewLine = Line[0];
+                    for (int i = 1; i < Line.Count; i++)
+                    {
+                        NewLine += "," + Line[i];
+                    }
+                    NewLine += "\n";
+                    Files[station].Add(NewLine);
+                    backgroundWorker2.ReportProgress(++RPG);
+                }
+            }
+            foreach (string F in Files.Keys)
+            {
+                FileStream FS = File.Create(COFD.FileName + @"\" + OPdate + "_" + F + ".csv");
+                StreamWriter SW = new StreamWriter(FS, Encoding.GetEncoding("big5"));
+                foreach(string OP in Files[F]) SW.Write(OP);
+
+                SW.Close();
+                backgroundWorker2.ReportProgress(++RPG);
+            }   
+            OPdate = "";
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            COFD.InitialDirectory=@"C:\";
+            COFD.IsFolderPicker = true;
+            COFD.Title = "選擇儲存路徑";
+            OPstation.Clear();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++) if (checkedListBox1.GetItemChecked(i))OPstation.Add(checkedListBox1.Items[i].ToString());
+                       
+            if (COFD.ShowDialog() == CommonFileDialogResult.Ok)
+            {  
+                int Process = 0;
+                foreach (string s in OPstation) Process += TDATA[s].Count;
+                progressBar1.Maximum = Process + OPstation.Count;
+                progressBar1.Value = 0;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            else MessageBox.Show("輸出已取消", "Transer");
+            button2.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker BW = (BackgroundWorker)sender;
+            Transer(BW, e);
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("轉換完成，已可輸出", "Transer");
+            button5.Enabled = true;
+            listBox1.Items.Clear();
+            button2.Enabled = true;
+            checkedListBox1.Items.Clear();
+           foreach(string station in TDATA.Keys) checkedListBox1.Items.Add(station);
+        }
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker BW = (BackgroundWorker)sender;
+            Outputer(BW, e);
+        }
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("輸出已完成", "Transer");
+            
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ReadFile = new List<string>();
+
+            if (listBox1.Items.Count > 0)
+            {
+
+                foreach (string IPFname in listBox1.Items)
+                {
+                    Encoding ec = Encoding.GetEncoding("big5");
+                    List<string> Temp = File.ReadAllLines(IPFname, ec).ToList();
+                   Temp.RemoveAt(0);
+                    ReadFile = ReadFile.Concat(Temp).ToList();
+                    
+                }
+
+                progressBar1.Maximum = ReadFile.Count();
+                progressBar1.Value = 0;
+                backgroundWorker1.RunWorkerAsync();
+                button5.Enabled = false;
+            }
+            else MessageBox.Show("輸入檔案為空", "Transer");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "選擇CSV檔案";
+            ofd.Filter = "CSV Files (.csv)|*.csv|All Files(*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (!listBox1.Items.Contains(ofd.FileName)) listBox1.Items.Add(ofd.FileName);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            for(int i=0;i<checkedListBox1.Items.Count;i++)
+            {
+                checkedListBox1.SetItemChecked(i, checkBox1.Checked);
+            }
+
+        }
+
+    }
+}
